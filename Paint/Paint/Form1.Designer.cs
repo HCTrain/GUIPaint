aab451d15c4e77fe097ac2b33feb8937d6a48af8
HCTrain <91628931+HCTrain@users.noreply.github.com>
2022-03-03 14:47:09 +0000

    Add files via upload

---
+++ Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Paint
+{
+    partial class Paint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSizeSmall = new System.Windows.Forms.Button();
+            this.btnSizeBig = new System.Windows.Forms.Button();
+            this.btnChangeColour = new System.Windows.Forms.Button();
+            this.trckbarChangeSize = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trckbarChangeSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSizeSmall
+            // 
+            this.btnSizeSmall.Location = new System.Drawing.Point(13, 13);
+            this.btnSizeSmall.Name = "btnSizeSmall";
+            this.btnSizeSmall.Size = new System.Drawing.Size(115, 21);
+            this.btnSizeSmall.TabIndex = 0;
+            this.btnSizeSmall.Text = "Small Brush";
+            this.btnSizeSmall.UseVisualStyleBackColor = true;
+            this.btnSizeSmall.Click += new System.EventHandler(this.btnSizeSmall_Click);
+            // 
+            // btnSizeBig
+            // 
+            this.btnSizeBig.Location = new System.Drawing.Point(12, 40);
+            this.btnSizeBig.Name = "btnSizeBig";
+            this.btnSizeBig.Size = new System.Drawing.Size(116, 22);
+            this.btnSizeBig.TabIndex = 1;
+            this.btnSizeBig.Text = "Big Brush";
+            this.btnSizeBig.UseVisualStyleBackColor = true;
+            this.btnSizeBig.Click += new System.EventHandler(this.btnSizeBig_Click);
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnChangeColour.Location = new System.Drawing.Point(13, 68);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(116, 23);
+            this.btnChangeColour.TabIndex = 2;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
+            // 
+            // trckbarChangeSize
+            // 
+            this.trckbarChangeSize.Location = new System.Drawing.Point(158, 13);
+            this.trckbarChangeSize.Maximum = 100;
+            this.trckbarChangeSize.Name = "trckbarChangeSize";
+            this.trckbarChangeSize.Size = new System.Drawing.Size(104, 45);
+            this.trckbarChangeSize.TabIndex = 3;
+            this.trckbarChangeSize.TickFrequency = 10;
+            this.trckbarChangeSize.Scroll += new System.EventHandler(this.trckbarChangeSize_Scroll);
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 402);
+            this.Controls.Add(this.trckbarChangeSize);
+            this.Controls.Add(this.btnChangeColour);
+            this.Controls.Add(this.btnSizeBig);
+            this.Controls.Add(this.btnSizeSmall);
+            this.Name = "Paint";
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Paint_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trckbarChangeSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSizeSmall;
+        private System.Windows.Forms.Button btnSizeBig;
+        private System.Windows.Forms.Button btnChangeColour;
+        private System.Windows.Forms.TrackBar trckbarChangeSize;
+    }
+}
+
---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Paint : Form
+    {
+        int brushSize = 10;
+        int red = 250;
+        int green = 0;
+        int blue = 0;
+        bool redcycle = true;
+        
+        public Paint()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Paint_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left )
+            {
+                if (redcycle == true)
+                {
+                    red = red + 5;
+                    if (red > 255)
+                    {
+                        red = 0;
+                    }
+
+                    System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(255, red, 0, 0));
+                    this.CreateGraphics().FillEllipse(brush, e.X, e.Y, brushSize, brushSize);
+                } 
+                else
+                {
+                    System.Drawing.SolidBrush brush = new System.Drawing.SolidBrush(Color.FromArgb(255, red, 0, 0));
+                    this.CreateGraphics().FillEllipse(brush, e.X, e.Y, brushSize, brushSize);
+              }
+            }
+        }
+
+        private void btnSizeSmall_Click(object sender, EventArgs e)
+        {
+            brushSize = 10;
+        }
+
+        private void btnSizeBig_Click(object sender, EventArgs e)
+        {
+            brushSize = 30;
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            
+                Random r = new Random(256);
+                red = r.Next(0, 256);
+                blue = r.Next(0, 256);
+                green = r.Next(0, 256);  
+        }
+
+        private void trckbarChangeSize_Scroll(object sender, EventArgs e)
+        {
+            brushSize = trckbarChangeSize.Value;
+        }
+    }
+}
